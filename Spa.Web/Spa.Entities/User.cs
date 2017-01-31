7e227eeb1f06a55fdd402489f20fd55a965d3159
bhattachansamrat <bhattachansamrat@gmail.com>
2017-01-31 21:21:19 +0545

    added entities

---
+++ Spa.Web/Spa.Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spa.Entities
+{
+    public class User : IEntityBase
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string HashedPassword { get; set; }
+        public string PasswordSalt { get; set; }
+        public string Email { get; set; }
+        public bool IsLocked { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ Spa.Web/Spa.Entities/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spa.Entities
+{
+    public class UserRole: IEntityBase
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
